d51ed920e8faf012f779bbb0697920161584530c
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:48:47 -0800

    Update P328.cs

--- P328.cs
+++ P328.cs
@@ -1,4 +1,15 @@
-﻿using System;
+/*
+3.28 (Digits of an Integer) Write an app that inputs one number consisting of five digits from the user, 
+separates the number into its individual digits and displays the digits separated from one another by
+three spaces each. For example, if the user types 42339, the app should display
+4 2 3 3 9
+Assume that the user enters the correct number of digits. What happens when you execute the app and type
+a number with more than five digits? What happens when you execute the app and type a number with fewer
+than five digits? [Hint: It’s possible to do this exercise with the techniques you learned in this chapter. 
+You’ll need to use both division and remainder operations to “pick off” each digit.]
+*/
+
+using System;
 
 class MainClass
 {
