599e6318de0a72cdae7327e54a5c779bcb3b7ae7
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:51:21 -0800

    Update P331.cs

--- P331.cs
+++ P331.cs
@@ -1,4 +1,21 @@
-﻿using System;
+/*
+3.31 (Body Mass Index Calculator) The formulas for calculating the BMI are 
+BMI=weightInPounds×703heightInInches × heightInInches
+or
+BMI=weightInKilogramsheightInMeters × heightInMeters
+Create a BMI calculator app that reads the user’s weight in pounds and height in inches (or, if you prefer, 
+the user’s weight in kilograms and height in meters), then calculates and displays the user’s body mass index.
+The app should also display the following information from the Department of Health and Human Services/National 
+Institutes of Health so the user can evaluate his/her BMI:
+
+BMI VALUES
+Underweight:  less than 18.5
+Normal:       between 18.5 and 24.9
+Overweight:   between 25 and 29.9
+Obese:        30 or greater
+*/
+
+using System;
 
 
 class practice
