38be000930bd38d469b0663abaaedbddecc8bea0
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:52:55 -0800

    Update P332.cs

--- P332.cs
+++ P332.cs
@@ -1,4 +1,20 @@
-﻿using System;
+/*
+3.32 (Car-Pool Savings Calculator) Research several car-pooling websites. Create an app that calculates your daily driving cost, 
+so that you can estimate how much money could be saved by car pooling, which also has other advantages such as reducing carbon
+emissions and reducing traffic congestion. The app should input the following information and display the user’s cost per day of 
+driving to work:
+
+a. Total miles driven per day.
+
+b. Cost per gallon of gasoline (in cents).
+
+c. Average miles per gallon.
+
+d. Parking fees per day (in cents).
+
+e. Tolls per day (in cents).
+*/
+using System;
 
 class MainClass
 {
