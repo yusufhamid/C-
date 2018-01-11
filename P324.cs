52ff9dc3f98d4d3f7cde509a3695d9e59cf0647d
Yusuf Hamid <yusufh97@live.com>
2018-01-11 13:58:05 -0800

    Add files via upload

---
+++ P324.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1;
+           
+            Console.WriteLine("Enter an integer: ");
+            integer1= int .Parse(Console.ReadLine());
+           
+           
+          
+            if (integer1 % 2 == 0 )
+            {
+                Console.WriteLine("the integer is even");
+                Console.ReadLine();
+            } 
+            else
+            {
+                Console.WriteLine("the integer is odd");
+                Console.ReadLine();
+            }
+        }
+    }
+}
