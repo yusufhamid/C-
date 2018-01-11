15262fcfcf3132daf36dc9afad0485cb34b27e3e
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:02:59 -0800

    Add files via upload

---
+++ p616.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //declare variables 
+        int i;
+        int k;
+        int number1;
+        int number2;
+        int number3;
+        //ask the user to enter 3 numbers
+        Console.WriteLine("Enter the number between 1 to 30: ");
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number between 1 to 30: ");
+        number2 = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Enter the third number between 1 to 30: ");
+        number3 = int.Parse(Console.ReadLine());
+
+        //runs until the exception is reached
+        for (i = 1; i <= 1; i++)
+        {
+            if (number1 > 30 || number2 > 30 || number3 > 30 || number1 < 1 || number2 < 1 || number3 < 1)
+            {
+                Console.WriteLine("The number you entered is not between 1 to 30. Re enter:");
+
+            }
+            else
+            {
+                //prints stars for the number that are entered
+                for (k = 1; k <= number1; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+                for (k = 1; k <= number2; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+                for (k = 1; k <= number3; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
