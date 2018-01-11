4cd7bb79a0d74a44e38bd10ad42eb5b242b7d793
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:56:28 -0800

    Update p616.cs

--- p616.cs
+++ p616.cs
@@ -1,4 +1,8 @@
-﻿
+/*
+6.16 (Displaying a Bar Chart) One interesting application of computers is to display graphs and bar charts.
+Write an app that reads three numbers between 1 and 30. For each number that’s read, your app should display
+the same number of adjacent asterisks. For example, if your app reads the number 7, it should display *******.
+*/
 using System;
 class MainClass
 {
