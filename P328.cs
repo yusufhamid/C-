485f707950956543264f8d52e08c4ba5a8c038f2
Yusuf Hamid <yusufh97@live.com>
2018-01-11 13:59:05 -0800

    Add files via upload

---
+++ P328.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main()
+    {
+        int number;
+        int digit0;
+        int digit1;
+        int digit2;
+        int digit3;
+        int digit4;
+
+
+        Console.Write("Enter any four digit number: ");
+        number = int.Parse(Console.ReadLine());
+
+        digit4 = number % 10;
+        digit3 = (number % 100) / 10;
+        digit2 = (number % 1000) / 100;
+        digit1 = (number % 10000) / 1000;
+        digit0 = (number % 100000) / 10000;
+
+
+        Console.WriteLine($"Here:  {digit0}   {digit1}   {digit2}   {digit3}   {digit4}");
+
+        Console.Write($"Or other ways \n {digit0} \n {digit1} \n {digit2} \n {digit3} \n {digit4}");
+        Console.Read();
+    }
+}
