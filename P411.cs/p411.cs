0178a2bddcc3e33673196541cccfe47a92e4a5db
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:55:14 -0800

    Update p411.cs

--- P411.cs/p411.cs
+++ P411.cs/p411.cs
@@ -1,4 +1,13 @@
-﻿
+/*
+4.11 (Employee Class) Create a class called Employee that includes three pieces of information as either
+instance variables or auto-implemented properties—a first name (type string), a last name (type string) 
+and a monthly salary (decimal). Your class should have a constructor that initializes the three values.
+Provide a property with a get and set accessor for any instance variables. If the monthly salary is negative,
+the set accessor should leave the instance variable unchanged. Write a test app named EmployeeTest that 
+demonstrates class Employee’s capabilities. Create two Employee objects and display each object’s yearly salary. 
+Then give each Employee a 10% raise and display each Employee’s yearly salary again.
+*/
+
 using System;
 /****Create and test Employee class ****/
 namespace p411.cs //define name
