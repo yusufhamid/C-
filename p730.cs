8ab5e861e4070b5a1e269676f50757ae84832faa
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:01:41 -0800

    Add files via upload

---
+++ p730.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int rnumber;
+        int guessnumber;
+        //selects random number between 1-1000
+        Random randomNumbers = new Random();
+        rnumber = randomNumbers.Next(1, 1001);
+
+        //user enters a number to get the random number selected
+        Console.WriteLine("Guess a number between 1 and 1000");
+        guessnumber = int.Parse(Console.ReadLine());
+
+        //loop exception 
+        while (guessnumber != rnumber)
+        {
+            if (guessnumber > rnumber)
+            {
+                Console.WriteLine("Too high. Try again");
+                Console.WriteLine("Guess a number between 1 and 1000 again!");
+                guessnumber = int.Parse(Console.ReadLine());
+            }
+
+            else if (guessnumber < rnumber)
+            {
+                Console.WriteLine("Too low. Try again");
+                Console.WriteLine("Guess a number between 1 and 1000 again!");
+                guessnumber = int.Parse(Console.ReadLine());
+            }
+        }
+        if (guessnumber == rnumber)
+        {
+            Console.WriteLine("Congratualations. You guessed the number!");
+        }
+        Console.ReadLine();
+    }
+}
+
