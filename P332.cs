3f05a13bb5564a0d7993a7ebafe925d55dddfc2f
Yusuf Hamid <yusufh97@live.com>
2018-01-11 14:00:36 -0800

    Add files via upload

---
+++ P332.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main()
+    {
+        double dailydrivingcost;
+        double totalmiles;
+        double gasolinecost;
+        double milespergallon;
+        double parkingfees;
+        double tolls;
+
+        Console.WriteLine("Enter total miles driven per day: ");
+        totalmiles = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter cost per gallon of gasoline (in cent: ");
+        gasolinecost = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter average miles per gallon: ");
+        milespergallon = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter parking fees per day (in cents): ");
+        parkingfees = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter tolls per day (in cents): ");
+        tolls = double.Parse(Console.ReadLine());
+
+        dailydrivingcost = (totalmiles / milespergallon) * gasolinecost + parkingfees + tolls;
+
+        Console.WriteLine($"Your daily driving cost is ${dailydrivingcost}");
+
+
+        if (dailydrivingcost > 4)
+        {
+            Console.WriteLine("Save money. Consider car pooling!!!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("You don't need carpooling.!!!");
+            Console.ReadLine();
+        }
+    }
+}
